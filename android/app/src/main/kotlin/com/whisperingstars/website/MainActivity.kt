3fbb99eb6fcc95b7c0975c663f144057a0c0b4fa
François Grèzes <grezesfrancois@gmail.com>
2023-12-27 00:26:29 +0100

    init flutter project

---
+++ android/app/src/main/kotlin/com/whisperingstars/website/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.whisperingstars.website.whispering_stars_website
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:whispering_stars_website/app/app.dart';
+import 'package:whispering_stars_website/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
